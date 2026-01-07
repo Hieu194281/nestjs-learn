1ccb23607c02444774ed8610649e68cc0008d2de
Nguyen Trung Hieu <hieunt115@Mac-mini-cua-Nguyen.local>
2026-01-07 17:32:33 +0700

    init commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const configService = new ConfigService();
+  const port = configService.get('PORT') || 3000;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+}
+bootstrap();
